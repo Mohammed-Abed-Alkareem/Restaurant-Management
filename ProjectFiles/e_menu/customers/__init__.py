4a46476179300503c1451a21747876259b83ead9
Mohammed Abed Alkareem <136195219+Mohammed-Abed-Alkareem@users.noreply.github.com>
2024-03-18 15:08:40 +0200

    Delete ProjectFiles directory

--- ProjectFiles/e_menu/customers/__init__.py
+++
@@ -1,6 +0,0 @@
-from flask import Blueprint
-
-customers = Blueprint('customers', __name__, url_prefix="/customer")
-
-from .customers import customers
-
--- ProjectFiles/e_menu/managers/__init__.py
+++
@@ -1,5 +0,0 @@
-from flask import Blueprint
-
-managers = Blueprint('managers', __name__, url_prefix='/manager')
-
-from .managers import *  # Import routes from manager.py
